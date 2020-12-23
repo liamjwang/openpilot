443f97819e29d431f357b65af214b88c262cb9be
Liam Wang <5054950+wangl5@users.noreply.github.com>
2020-12-22 19:27:17 -0500

    Add custom jerk limits to long

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -27,7 +27,16 @@ _A_CRUISE_MIN_BP = [  0.,  5.,  10., 20.,  40.]
 # make sure these accelerations are smaller than mpc limits
 _A_CRUISE_MAX_V = [1.2, 1.2, 0.65, .4]
 _A_CRUISE_MAX_V_FOLLOWING = [1.6, 1.6, 0.65, .4]
-_A_CRUISE_MAX_BP = [0.,  6.4, 22.5, 40.]
+_A_CRUISE_MAX_BP = [0.,  6.4, 22.5, 40.] # someone seems to think this is kph, but isn't this in m/s? (6.4kph ~ 4mph)
+
+# lookup tables VS speed to determine min and max jerk in cruise
+_J_CRUISE_MIN_V = [-.8, -.7, -.4, -.2, -.2]
+_J_CRUISE_MIN_BP = [  0.,  5.,  10., 20.,  40.]
+
+# need fast jerk at very low speed for stop and go
+_J_CRUISE_MAX_V = [.8, .7, .4, .2, .2] # slow at first for safety
+_J_CRUISE_MAX_V_FOLLOWING = [1.0, 1.1, .4, .2, .2]
+_J_CRUISE_MAX_BP = [ 0.,  5.,  10., 20.,  40.]
 
 # Lookup table for turns
 _A_TOTAL_MAX_V = [1.7, 3.2]
@@ -41,7 +50,16 @@ def calc_cruise_accel_limits(v_ego, following):
     a_cruise_max = interp(v_ego, _A_CRUISE_MAX_BP, _A_CRUISE_MAX_V_FOLLOWING)
   else:
     a_cruise_max = interp(v_ego, _A_CRUISE_MAX_BP, _A_CRUISE_MAX_V)
-  return np.vstack([a_cruise_min, a_cruise_max])
+  return [a_cruise_min, a_cruise_max]
+
+def calc_cruise_jerk_limits(v_ego, following):
+  j_cruise_min = interp(v_ego, _J_CRUISE_MIN_BP, _J_CRUISE_MIN_V)
+
+  if following:
+    j_cruise_max = interp(v_ego, _J_CRUISE_MAX_BP, _J_CRUISE_MAX_V_FOLLOWING)
+  else:
+    j_cruise_max = interp(v_ego, _J_CRUISE_MAX_BP, _J_CRUISE_MAX_V)
+  return [j_cruise_min, j_cruise_max]
 
 
 def limit_accel_in_turns(v_ego, angle_steers, a_target, CP):
@@ -124,8 +142,8 @@ class Planner():
 
     # Calculate speed for normal cruise control
     if enabled and not self.first_loop and not sm['carState'].gasPressed:
-      accel_limits = [float(x) for x in calc_cruise_accel_limits(v_ego, following)]
-      jerk_limits = [min(-0.1, accel_limits[0]), max(0.1, accel_limits[1])]  # TODO: make a separate lookup for jerk tuning
+      accel_limits = calc_cruise_accel_limits(v_ego, following)
+      jerk_limits = calc_cruise_jerk_limits(v_ego, following)
       accel_limits_turns = limit_accel_in_turns(v_ego, sm['carState'].steeringAngle, accel_limits, self.CP)
 
       if force_slow_decel:
