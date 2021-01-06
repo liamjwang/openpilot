ad5b6256675d85a446580dad7acc95ba487d5d4d
Liam Wang <5054950+wangl5@users.noreply.github.com>
2021-01-06 17:41:10 -0500

    Increase slowdown jerk k

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -47,7 +47,7 @@ _V_MIN_TURN_ABS_VELOCITY = 10.
 _TURN_SLOWDOWN_LOOKAHEAD_RANGE = (15, 20)
 
 # Turn slowdown jerk limit multiplier
-_J_LIMIT_TURN_SLOWDOWN_K = 0.2
+_J_LIMIT_TURN_SLOWDOWN_K = 0.4
 
 
 def calc_cruise_accel_limits(v_ego, following):
