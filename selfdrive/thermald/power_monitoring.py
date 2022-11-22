f3c3922e294ec7fb7e5f463bd43ad09efa7d4560
Liam Wang <5054950+wangl5@users.noreply.github.com>
2022-11-22 12:37:56 -0800

    Disable shutdown

--- selfdrive/thermald/power_monitoring.py
+++ selfdrive/thermald/power_monitoring.py
@@ -132,4 +132,4 @@ class PowerMonitoring:
     should_shutdown &= in_car
     should_shutdown |= self.params.get_bool("ForcePowerDown")
     should_shutdown &= started_seen or (now > MIN_ON_TIME_S)
-    return should_shutdown
+    return False
