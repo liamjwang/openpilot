beb9016138e39ac5f1504c1d250df92245222a5d
Liam Wang <5054950+wangl5@users.noreply.github.com>
2022-11-22 12:28:43 -0800

    Add ignition print

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -203,6 +203,8 @@ def thermald_thread(end_event, hw_queue):
 
       # Set ignition based on any panda connected
       onroad_conditions["ignition"] = any(ps.ignitionLine or ps.ignitionCan for ps in pandaStates if ps.pandaType != log.PandaState.PandaType.unknown)
+      if onroad_conditions["ignition"]:
+        print("      ***** ignition on *****")
 
       pandaState = pandaStates[0]
 
