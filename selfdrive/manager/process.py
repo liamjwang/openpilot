3b134546f3184ae077ec01bc95377dc2ef705d4f
Liam Wang <5054950+wangl5@users.noreply.github.com>
2022-11-23 18:02:40 -0800

    Add process start log

--- selfdrive/manager/process.py
+++ selfdrive/manager/process.py
@@ -311,6 +311,7 @@ def ensure_running(procs: ValuesView[ManagerProcess], started: bool, params=None
 
     if run:
       p.start()
+      print(f"starting {p.name}, {time.time()}")
     else:
       p.stop(block=False)
 
