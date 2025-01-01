522c9e3e908171eab709a54012be92b0d7d390f4
Liam Wang <5054950+liamjwang@users.noreply.github.com>
2024-12-31 23:10:32 -0500

    Disable shutdown

--- system/hardware/power_monitoring.py
+++ system/hardware/power_monitoring.py
@@ -125,4 +125,5 @@ class PowerMonitoring:
     should_shutdown &= offroad_time > DELAY_SHUTDOWN_TIME_S
     should_shutdown |= self.params.get_bool("ForcePowerDown")
     should_shutdown &= started_seen or (now > MIN_ON_TIME_S)
-    return should_shutdown
+    # return should_shutdown
+    return False
