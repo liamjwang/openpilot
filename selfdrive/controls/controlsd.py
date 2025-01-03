3e97631235806082ad4663f1f7622728b70cbded
Liam Wang <5054950+liamjwang@users.noreply.github.com>
2025-01-03 12:50:04 -0500

    Update

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -60,6 +60,8 @@ ENABLED_STATES = (State.preEnabled, *ACTIVE_STATES)
 
 class Controls:
   def __init__(self, CI=None):
+    print("liam controlsd __init__ ", time.monotonic())
+    self.first_step = True
     self.params = Params()
 
     if CI is None:
@@ -811,6 +813,10 @@ class Controls:
     self.publish_logs(CS, start_time, CC, lac_log)
 
     self.CS_prev = CS
+
+    if self.first_step:
+      self.first_step = False
+      print("liam controlsd first step done ", time.monotonic())
 
   def read_personality_param(self):
     try:
