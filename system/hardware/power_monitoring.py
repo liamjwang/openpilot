e0205dedb3ba3e764a36e5a609f045c9317fc72b
Liam Wang <5054950+liamjwang@users.noreply.github.com>
2025-01-19 14:12:38 -0500

    Disable low voltage shutdown

--- system/hardware/power_monitoring.py
+++ system/hardware/power_monitoring.py
@@ -114,10 +114,10 @@ class PowerMonitoring:
     now = time.monotonic()
     should_shutdown = False
     offroad_time = (now - offroad_timestamp)
-    low_voltage_shutdown = (self.car_voltage_mV < (VBATT_PAUSE_CHARGING * 1e3) and
-                            offroad_time > VOLTAGE_SHUTDOWN_MIN_OFFROAD_TIME_S)
+    # low_voltage_shutdown = (self.car_voltage_mV < (VBATT_PAUSE_CHARGING * 1e3) and
+    #                         offroad_time > VOLTAGE_SHUTDOWN_MIN_OFFROAD_TIME_S)
     should_shutdown |= offroad_time > MAX_TIME_OFFROAD_S
-    should_shutdown |= low_voltage_shutdown
+    # should_shutdown |= low_voltage_shutdown
     should_shutdown |= (self.car_battery_capacity_uWh <= 0)
     should_shutdown &= not ignition
     should_shutdown &= (not self.params.get_bool("DisablePowerDown"))
